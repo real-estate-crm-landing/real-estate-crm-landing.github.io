290c3af793423d52378c7339847b1d80a712fedb
Mirakhmedov Mirsaid <66273717+mirsaid-number1@users.noreply.github.com>
2023-11-29 20:32:13 +0500

    deploy

--- _next/static/T0t-h2MWiOxb-vehbMKZL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[{has:void 0,source:"/github-pages/:lng/page/:slug",destination:"/github-pages/page?lng=:lng&slug=:slug"}],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-1a9fa3ac20787313.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
